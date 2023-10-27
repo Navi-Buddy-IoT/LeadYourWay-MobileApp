ddb100d7b70aa7e3ed28f82e11066c069d0d7e38
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-10-27 10:14:22 -0500

    feat: signup template

--- lib/auth/screens/signup.dart
+++ lib/auth/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lead_your_way/shared/widgets/lyw_rounded_input_filed.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -10,9 +11,45 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("Welcome to sign up"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo-lyw.png'),
+            const SizedBox(height: 32),
+            const Text(
+              "Create Account",
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 32),
+            const RoundedInputField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              hintText: "Your email",
+              icon: Icon(Icons.person),
+            ),
+            RoundedInputFieldObscure(
+              keyboardType: TextInputType.visiblePassword,
+              textInputAction: TextInputAction.done,
+              hintText: "Your password",
+              icon: const Icon(Icons.lock),
+            ),
+            const SizedBox(height: 32),
+            FilledButton(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                backgroundColor: Colors.orangeAccent,
+                foregroundColor: Colors.white,
+                fixedSize: const Size(200, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(32),
+                ),
+              ),
+              child: const Text("Login"),
+            ),
+          ],
+        ),
       ),
     );
   }
