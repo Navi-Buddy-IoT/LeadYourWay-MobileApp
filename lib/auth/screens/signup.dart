26556ae44835524fdc141f82140d0055057d6775
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-10-27 10:44:36 -0500

    feat: added toast to signup

--- lib/auth/screens/signup.dart
+++ lib/auth/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:lead_your_way/shared/widgets/lyw_rounded_input_filed.dart';
 
 class SignUp extends StatefulWidget {
@@ -11,11 +12,14 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController passwordConfirmationController =
+      TextEditingController();
 
   @override
   void dispose() {
     emailController.dispose();
     passwordController.dispose();
+    passwordConfirmationController.dispose();
     super.dispose();
   }
 
@@ -47,9 +51,33 @@ class _SignUpState extends State<SignUp> {
               icon: const Icon(Icons.lock),
               controller: passwordController,
             ),
+            RoundedInputFieldObscure(
+              keyboardType: TextInputType.visiblePassword,
+              textInputAction: TextInputAction.done,
+              hintText: "Confirm Password",
+              icon: const Icon(Icons.lock),
+              controller: passwordConfirmationController,
+            ),
             const SizedBox(height: 32),
             FilledButton(
-              onPressed: () {},
+              onPressed: () {
+                // final email = emailController.text;
+                final password = passwordController.text;
+                final passwordConfirmation =
+                    passwordConfirmationController.text;
+
+                if (password != passwordConfirmation) {
+                  Fluttertoast.showToast(
+                    msg: "Your password does not match",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.CENTER,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.orangeAccent,
+                    textColor: Colors.white,
+                    fontSize: 16.0,
+                  );
+                }
+              },
               style: FilledButton.styleFrom(
                 backgroundColor: Colors.orangeAccent,
                 foregroundColor: Colors.white,
@@ -58,7 +86,7 @@ class _SignUpState extends State<SignUp> {
                   borderRadius: BorderRadius.circular(32),
                 ),
               ),
-              child: const Text("Login"),
+              child: const Text("Sign Up"),
             ),
           ],
         ),
