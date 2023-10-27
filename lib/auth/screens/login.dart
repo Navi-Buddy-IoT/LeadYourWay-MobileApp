9cc21337bdbe36fe71602283fffaa668e9704cd4
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-10-27 08:31:35 -0500

    feat: added input fields

--- lib/auth/screens/login.dart
+++ lib/auth/screens/login.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:lead_your_way/renting/screens/home_page.dart';
+import 'package:lead_your_way/shared/widgets/lyw_rounded_input_filed.dart';
 
 class Login extends StatefulWidget {
-  const Login({ Key? key }) : super(key: key);
+  const Login({Key? key}) : super(key: key);
 
   @override
   State<Login> createState() => _LoginState();
@@ -10,26 +11,46 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Welcome to login", textAlign: TextAlign.center,),
-            const SizedBox(height: 32,),
+            const Text(
+              "Welcome to login",
+              style: TextStyle(fontSize: 24),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 32,
+            ),
             GestureDetector(
               onTap: () {
                 Navigator.push(
-                  context, 
-                  MaterialPageRoute(builder: (context) => const HomePage())
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
                 );
               },
-              child: const Text("Go to Home")
-            )
+              child: const Text("Go to Home"),
+            ),
+            const RoundedInputField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              hintText: "Your email",
+              icon: Icon(Icons.person),
+            ),
+            RoundedInputFieldObscure(
+              keyboardType: TextInputType.visiblePassword,
+              textInputAction: TextInputAction.done,
+              hintText: "Your password",
+              icon: const Icon(Icons.lock),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
---
+++ lib/shared/widgets/lyw_tex_filed_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: Colors.orangeAccent,
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: child,
+    );
+  }
+}
