4a5969766a4eb486f0819881ee565e9355f7ef06
Andrés Leiva <aleiva1700@gmail.com>
2023-11-06 09:42:12 -0500

    fix: remove overflow on login and signup screens

--- lib/auth/screens/signup.dart
+++ lib/auth/screens/signup.dart
@@ -28,6 +28,7 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
